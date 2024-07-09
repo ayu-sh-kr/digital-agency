0246161291e5d747d77d4d0f04a7618225b1a145
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-10 01:44:26 +0530

    Update HeroSection

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -3,7 +3,13 @@
 </script>
 
 <template>
-
+<div class="flex flex-col items-center justify-center h-[80vh] font-tahoma gap-y-5">
+    <h1 class="text-gray-900 dark:text-gray-100 text-6xl tracking-wider">Stop Guessing</h1>
+    <h1 class="text-teal-600 text-7xl tracking-wide font-semibold">Embrace Digital Marketing as Science</h1>
+    <div>
+        <UButton label="Get Started" size="xl" />
+    </div>
+</div>
 </template>
 
 <style scoped>
