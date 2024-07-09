c45f08c335747ebb78548512350c5ed0c5df3f35
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-10 01:27:01 +0530

    Create default.vue: Default Layout of the App

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import AppHeader from "~/components/utils/AppHeader.vue";
+</script>
+
+<template>
+    <AppHeader/>
+    <slot/>
+    <UNotifications/>
+</template>
+
+<style scoped>
+
+</style>
