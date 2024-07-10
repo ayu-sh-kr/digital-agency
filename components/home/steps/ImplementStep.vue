bca696ef6a1474f871ef6228d738f8931318e93a
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-11 01:00:58 +0530

    Create Implementation.vue

---
+++ components/home/steps/ImplementStep.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <section class="px-3 py-20 font-tahoma">
+        <div class="text-center">
+            <h3 class="lg:text-3xl md:text-2xl text-xl"><span class="text-primary">Implement</span> Digital Marketing</h3>
+            <div class="py-16 grid md:grid-cols-2 grid-cols-1 place-items-center place-content-center ">
+                <div class="md:hidden block">
+                    <UIcon name="ic:twotone-settings-suggest" dynamic class="w-32 h-32 text-primary"/>
+                </div>
+                <div class="flex flex-col gap-y-5">
+                    <h1 class="font-semibold text-gray-800 dark:text-gray-200 md:text-xl text-lg">Implement Digital Marketing rules the correct way</h1>
+                    <p class="text-gray-900 dark:text-gray-100">
+                        Start by understanding what you want to achieve. Are you aiming for brand awareness,
+                        lead generation, sales, or customer retention? Set clear, measurable goals.
+
+                        Identify your target audience. Understand their demographics, interests, and online behavior. This knowledge will guide your marketing efforts.
+                    </p>
+                </div>
+                <div class="md:block hidden">
+                    <UIcon name="ic:twotone-settings-suggest" dynamic class="w-32 h-32 text-primary"/>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
