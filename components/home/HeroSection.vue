f4ce4be0631b4fb4a524e7e1eb4c24d7eef31f03
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-10 11:45:33 +0530

    UI fix on HeroSection

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -3,14 +3,14 @@
 </script>
 
 <template>
-<div class="flex flex-col items-center justify-center md:h-[80vh] sm:h-[70vh] h-[60vh] font-tahoma gap-y-5 px-3">
+<section class="flex flex-col items-center justify-center md:h-[80vh] sm:h-[70vh] h-[80vh] font-tahoma gap-y-5 px-3 py-20 sm:py-16 md:py-12">
     <h1 class="text-gray-700 dark:text-gray-300 md:text-6xl sm:text-5xl text-4xl tracking-wider text-center">Stop Guessing</h1>
-    <h1 class="text-teal-600 md:text-7xl sm:text-6xl text-5xl tracking-wide font-semibold text-center">Embrace Digital Marketing as Science</h1>
+    <h1 class="text-primary md:text-7xl sm:text-6xl text-5xl tracking-wide font-semibold text-center">Embrace Digital Marketing as Science</h1>
     <p class="text-center text-lg text-gray-800 dark:text-gray-200">We offer variety of resources to unlock your potential. Your #1 resource for digital marketing tips, trends and strategy to help you build a successful online business</p>
     <div>
         <UButton label="Get Started" size="xl" />
     </div>
-</div>
+</section>
 </template>
 
 <style scoped>
