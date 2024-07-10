89f5a2df7c991224b2ba2abd541b3edcb660e4d3
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-10 11:45:55 +0530

    Update index.vue: added OfferSection on index/home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HeroSection from "~/components/home/HeroSection.vue";
+import OfferSection from "~/components/home/OfferSection.vue";
 </script>
 
 <template>
     <HeroSection/>
+    <OfferSection/>
 </template>
 
 <style scoped>
