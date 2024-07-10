2ce9faa3246c397a7000ff9e874ce05706bce698
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-11 01:01:22 +0530

    Create LearnStep

---
+++ components/home/steps/LearnStep.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<section class="px-3 py-20 font-tahoma">
+    <div class="text-center">
+        <h3 class="lg:text-3xl md:text-2xl text-xl"><span class="text-primary">Learn</span> Digital Marketing</h3>
+        <div class="py-16 grid md:grid-cols-2 grid-cols-1 place-items-center place-content-center">
+            <div>
+                <UIcon name="ic:baseline-lightbulb" dynamic class="w-32 h-32 text-primary"/>
+            </div>
+            <div class="flex flex-col gap-y-5">
+                <h1 class="font-semibold text-gray-800 dark:text-gray-200 md:text-xl text-lg">Learn Digital Marketing and boost your business growth</h1>
+                <p class="text-gray-900 dark:text-gray-100">
+                    Digital marketing helps businesses reach wider audiences, build brand loyalty, and measure success effectively.
+                    Digital marketing lets you be creative. Crafting engaging content, designing campaigns,
+                    and experimenting with strategies allow you to express your creativity while achieving business goals.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+</template>
+
+<style scoped>
+
+</style>
