8203e194b48457681b41def7352072a05260b711
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-11 01:02:16 +0530

    Added Steps Project

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import HeroSection from "~/components/home/HeroSection.vue";
 import OfferSection from "~/components/home/OfferSection.vue";
+import StepsSection from "~/components/home/StepsSection.vue";
 </script>
 
 <template>
     <HeroSection/>
     <OfferSection/>
+    <StepsSection/>
 </template>
 
 <style scoped>
