ffaa7927de24fe65a8415cf11fe93547ff932ce5
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-11 01:01:12 +0530

    Create StepSection

---
+++ components/home/StepsSection.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import LearnStep from "~/components/home/steps/LearnStep.vue";
+import EarnStep from "~/components/home/steps/EarnStep.vue";
+import ImplementStep from "~/components/home/steps/ImplementStep.vue";
+</script>
+
+<template>
+    <LearnStep/>
+    <ImplementStep/>
+    <EarnStep/>
+</template>
+
+<style scoped>
+
+</style>
