6da7f8f585e08b9dc3266be1c3d4b7cd325c4f3a
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-11 01:00:44 +0530

    Create EarnStep.vue: Component to render frontend steps of DM

---
+++ components/home/steps/EarnStep.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <section class="px-3 py-20 font-tahoma">
+        <div class="text-center">
+            <h3 class="lg:text-3xl md:text-2xl text-xl"><span class="text-primary">Earn From</span> Digital Marketing</h3>
+            <div class="py-16 grid md:grid-cols-2 grid-cols-1 place-items-center place-content-center">
+                <div>
+                    <UIcon name="ic:baseline-attach-money" dynamic class="w-32 h-32 text-primary"/>
+                </div>
+                <div class="flex flex-col gap-y-5">
+                    <h1 class="font-semibold text-gray-800 dark:text-gray-200 md:text-xl text-lg">Earn by creating and selling your own digital products</h1>
+                    <p class="text-gray-900 dark:text-gray-100">
+                        Provide digital marketing consulting to companies. Help them optimize their strategies and achieve better results.
+                        Help businesses set up and manage their online stores. Understand platforms like Shopify or WooCommerce.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
