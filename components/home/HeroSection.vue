0b9062498f54dfa95fe573f0e50456b009b46893
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-11 01:00:03 +0530

    Added VueUes motion to the site

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-<section class="flex flex-col items-center justify-center md:h-[80vh] sm:h-[70vh] h-[80vh] font-tahoma gap-y-5 px-3 py-20 sm:py-16 md:py-12">
+<section  v-motion
+          :initial="{ opacity: 0, y: 100 }"
+          :enter="{ opacity: 1, y: 0, scale: 1, transition: { repeatType: 'mirror' } }"
+          :delay="200"
+          :duration="1200"
+          class="flex flex-col items-center justify-center md:h-[80vh] sm:h-[70vh] h-[80vh] font-tahoma gap-y-5 px-3 py-20 sm:py-16 md:py-12">
     <h1 class="text-gray-700 dark:text-gray-300 md:text-6xl sm:text-5xl text-4xl tracking-wider text-center">Stop Guessing</h1>
     <h1 class="text-primary md:text-7xl sm:text-6xl text-5xl tracking-wide font-semibold text-center">Embrace Digital Marketing as Science</h1>
     <p class="text-center text-lg text-gray-800 dark:text-gray-200">We offer variety of resources to unlock your potential. Your #1 resource for digital marketing tips, trends and strategy to help you build a successful online business</p>
