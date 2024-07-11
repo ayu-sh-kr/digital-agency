bd18ff05f3333c9e4e50dae068ff97d8cc5330a1
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-11 13:48:05 +0530

    Update ImplementationStep: Update the UI

--- components/home/steps/ImplementStep.vue
+++ components/home/steps/ImplementStep.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
 
+import SectionHeader from "~/components/utils/SectionHeader.vue";
 </script>
 
 <template>
-    <section class="px-3 py-20 font-tahoma">
+    <section v-motion-pop-visible class="px-3 py-12 sm:py-16 md:py-20 font-tahoma">
         <div class="text-center">
-            <h3 class="lg:text-3xl md:text-2xl text-xl"><span class="text-primary">Implement</span> Digital Marketing</h3>
-            <div class="py-16 grid md:grid-cols-2 grid-cols-1 place-items-center place-content-center ">
+            <SectionHeader simple_text="Digital Marketing" color_text="Implement" />
+            <div class="py-16 grid md:grid-cols-2 grid-cols-1 place-items-center place-content-center gap-y-5">
                 <div class="md:hidden block">
                     <UIcon name="ic:twotone-settings-suggest" dynamic class="w-32 h-32 text-primary"/>
                 </div>
                 <div class="flex flex-col gap-y-5">
                     <h1 class="font-semibold text-gray-800 dark:text-gray-200 md:text-xl text-lg">Implement Digital Marketing rules the correct way</h1>
-                    <p class="text-gray-900 dark:text-gray-100">
+                    <p class="text-gray-900 dark:text-gray-100 sm:px-10 md:px-16">
                         Start by understanding what you want to achieve. Are you aiming for brand awareness,
                         lead generation, sales, or customer retention? Set clear, measurable goals.
 
