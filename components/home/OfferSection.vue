2cf14727953dd0ad9f70c3c4093e5fb4577fd875
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-11 13:47:06 +0530

    Update OfferSection.vue: Change the motion directive

--- components/home/OfferSection.vue
+++ components/home/OfferSection.vue
@@ -3,11 +3,7 @@
 </script>
 
 <template>
-<section  v-motion
-          :initial="{ opacity: 0, y: 100 }"
-          :enter="{ opacity: 1, y: 0, scale: 1, transition: { repeatType: 'mirror' } }"
-          :delay="200"
-          :duration="1200" class="px-3 grid md:grid-cols-2 grid-cols-1 place-content-center place-items-center font-tahoma py-20 gap-y-5">
+<section  v-motion-pop-visible class="px-3 grid md:grid-cols-2 grid-cols-1 place-content-center place-items-center font-tahoma py-20 gap-y-5">
     <div class="md:text-6xl sm:text-5xl text-4xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-teal-600 via-emerald-600 to-green-600 text-center tracking-wide">
         Take your offline business online only in 30 days.
     </div>
