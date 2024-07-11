dd518d9a90ac8e396ac37ee7348b9f50a2618a8d
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-11 13:48:35 +0530

    Abstraction for Section Header

---
+++ components/utils/SectionHeader.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    color_text: {
+        type: String,
+        required: true
+    },
+
+    simple_text: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+<div class="text-center font-tahoma">
+    <h3 class="lg:text-3xl md:text-2xl text-xl"><span class="text-primary">{{ color_text }}</span> {{ simple_text }}</h3>
+</div>
+</template>
+
+<style scoped>
+
+</style>
