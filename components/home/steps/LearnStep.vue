c100b28d39160f1f48b1079a2b451094dbff4a06
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-11 13:47:28 +0530

    Update: Abstract away the common functionality

--- components/home/steps/LearnStep.vue
+++ components/home/steps/LearnStep.vue
@@ -1,26 +1,21 @@
 <script setup lang="ts">
 
+import StepComponent, {type StepData} from "~/components/home/steps/StepComponent.vue";
+
+const item: StepData = {
+    header_color: 'Learn',
+    header_simple: 'Digital Marketing',
+    sub_header: 'Learn Digital Marketing and boost your business growth',
+    description: `Digital marketing helps businesses reach wider audiences, build brand loyalty, and measure success effectively.
+                    Digital marketing lets you be creative. Crafting engaging content, designing campaigns,
+                    and experimenting with strategies allow you to express your creativity while achieving business goals.`,
+    icon: 'i-heroicons-light-bulb'
+}
 </script>
 
 <template>
-<section class="px-3 py-20 font-tahoma">
-    <div class="text-center">
-        <h3 class="lg:text-3xl md:text-2xl text-xl"><span class="text-primary">Learn</span> Digital Marketing</h3>
-        <div class="py-16 grid md:grid-cols-2 grid-cols-1 place-items-center place-content-center">
-            <div>
-                <UIcon name="ic:baseline-lightbulb" dynamic class="w-32 h-32 text-primary"/>
-            </div>
-            <div class="flex flex-col gap-y-5">
-                <h1 class="font-semibold text-gray-800 dark:text-gray-200 md:text-xl text-lg">Learn Digital Marketing and boost your business growth</h1>
-                <p class="text-gray-900 dark:text-gray-100">
-                    Digital marketing helps businesses reach wider audiences, build brand loyalty, and measure success effectively.
-                    Digital marketing lets you be creative. Crafting engaging content, designing campaigns,
-                    and experimenting with strategies allow you to express your creativity while achieving business goals.
-                </p>
-            </div>
-        </div>
-    </div>
-</section>
+    <StepComponent :icon="item.icon" :description="item.description" :sub_header="item.sub_header" :header_simple="item.header_simple" :header_color="item.header_color" />
+
 </template>
 
 <style scoped>
