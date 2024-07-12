842d029377e19562528c5f4e20c28b71ac4b981c
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-12 16:19:49 +0530

    Added Card Section to Home Page

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,14 @@
 import HeroSection from "~/components/home/HeroSection.vue";
 import OfferSection from "~/components/home/OfferSection.vue";
 import StepsSection from "~/components/home/StepsSection.vue";
+import CourseSection from "~/components/home/CourseSection.vue";
 </script>
 
 <template>
     <HeroSection/>
     <OfferSection/>
     <StepsSection/>
+    <CourseSection/>
 </template>
 
 <style scoped>
