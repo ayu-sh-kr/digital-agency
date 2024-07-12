f144166ca3e8a27d782ab3fa727b77cb207544b1
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-12 16:19:13 +0530

    Create Card Section for Courses

---
+++ components/home/CourseSection.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import SectionHeader from "~/components/utils/SectionHeader.vue";
+import CourseCard, {type CourseData} from "~/components/home/course/CourseCard.vue";
+
+const items: CourseData[] = [
+    {
+        icon: 'mdi:account-card',
+        label: 'LokNeeti',
+        description: [
+            'Create your Digital Ecosystem in 12 Weeks',
+            'Content Creation Shaastra',
+            'Social Media Shaastra',
+            'Website & SEO Shaastra',
+            'PPC Advertising Shaastra & Much More'
+        ]
+    }
+]
+</script>
+
+<template>
+<section class="px-3 font-tahoma dark:bg-gray-950 bg-gray-200 py-10">
+    <SectionHeader simple_text="Offered" color_text="Courses"/>
+
+    <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 place-items-center">
+        <CourseCard v-for="item in items" :data="item"/>
+    </div>
+</section>
+</template>
+
+<style scoped>
+
+</style>
