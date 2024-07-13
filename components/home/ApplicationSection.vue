7fcdf9fa8d838ea4afeed0fd51c4d7b5cc6e7aa1
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-13 22:29:15 +0530

    New Component

---
+++ components/home/ApplicationSection.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import SectionHeader from "~/components/utils/SectionHeader.vue";
+</script>
+
+<template>
+<section class="px-3 py-10 font-tahoma">
+    <SectionHeader simple_text="Application" color_text="Digital Marketing" />
+</section>
+</template>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@ import HeroSection from "~/components/home/HeroSection.vue";
 import OfferSection from "~/components/home/OfferSection.vue";
 import StepsSection from "~/components/home/StepsSection.vue";
 import CourseSection from "~/components/home/CourseSection.vue";
+import ApplicationSection from "~/components/home/ApplicationSection.vue";
 </script>
 
 <template>
@@ -10,6 +11,7 @@ import CourseSection from "~/components/home/CourseSection.vue";
     <OfferSection/>
     <StepsSection/>
     <CourseSection/>
+    <ApplicationSection/>
 </template>
 
 <style scoped>
