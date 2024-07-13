b4909427c638fe8478e5109188a575295ebde196
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-13 21:38:05 +0530

    Update CourseCard for dynamic Card Color by props

--- components/home/course/CourseCard.vue
+++ components/home/course/CourseCard.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 
 import type {PropType} from "vue";
-import SectionHeader from "~/components/utils/SectionHeader.vue";
 
 const props = defineProps({
     data: {
         type: Object as PropType<CourseData>,
         required: true
+    },
+
+    color: {
+        type: String
+    },
+
+    dark: {
+        type: String
+    },
+
+    light: {
+        type: String
     }
 })
 
@@ -19,15 +30,18 @@ export interface CourseData {
 </script>
 
 <template>
-<div class="flex flex-col items-center justify-between rounded-xl px-3 py-4 bg-violet-200 dark:bg-violet-500 shadow-lg">
+<div class="flex flex-col items-center justify-between rounded-xl px-3 py-4 shadow-lg"
+     :class="color ? `bg-${color}-200 bg-${color}-500 ` : 'bg-violet-200 dark:bg-violet-900 ' +  light ? `${light} ` : ' ' + dark ? `${dark} ` : ' '"
+>
     <div class="py-12">
-        <Icon :name="data.icon" class="w-24 h-24 text-violet-400 dark:text-violet-900"/>
+        <Icon :name="data.icon" class="w-24 h-24"
+              :class=" light ? `${light} ` : ' ' + dark ? `${dark} ` : ' '"
+        />
     </div>
-    <h1 class="text-center text-violet-400 dark:text-violet-900 lg:text-4xl md:text-3xl">{{data.label}}</h1>
+    <h1 class="text-center lg:text-4xl md:text-3xl">{{data.label}}</h1>
     <ul type="disc" class="px-3 py-5">
         <li v-for="content in data.description" class="md:text-lg text-sm py-2 text-left w-full">{{content}}</li>
     </ul>
-    <UButton label="Join Now" color="blue" size="lg"/>
 </div>
 </template>
 
