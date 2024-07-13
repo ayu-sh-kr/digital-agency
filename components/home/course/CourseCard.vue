f0519570b48196d6d4450e462ae90f2258fcf863
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-13 20:18:19 +0530

    Update CourseCard: UI Fix

--- components/home/course/CourseCard.vue
+++ components/home/course/CourseCard.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 
 import type {PropType} from "vue";
-import CourseCard from "~/components/home/course/CourseCard.vue";
 import SectionHeader from "~/components/utils/SectionHeader.vue";
 
 const props = defineProps({
@@ -20,15 +19,15 @@ export interface CourseData {
 </script>
 
 <template>
-<div class="flex flex-col items-center justify-between bg-transparent backdrop-blur shadow-lg rounded-lg px-3 py-2">
+<div class="flex flex-col items-center justify-between rounded-xl px-3 py-4 bg-violet-200 dark:bg-violet-500 shadow-lg">
     <div class="py-12">
-        <Icon :name="data.icon" class="w-24 h-24 text-gray-600" />
+        <Icon :name="data.icon" class="w-24 h-24 text-violet-400 dark:text-violet-900"/>
     </div>
-    <SectionHeader simple_text="" :color_text="data.label"/>
+    <h1 class="text-center text-violet-400 dark:text-violet-900 lg:text-4xl md:text-3xl">{{data.label}}</h1>
     <ul type="disc" class="px-3 py-5">
-        <li v-for="content in data.description" class="text-lg py-2 text-gray-900 dark:text-gray-100 text-left w-full">{{content}}</li>
+        <li v-for="content in data.description" class="md:text-lg text-sm py-2 text-left w-full">{{content}}</li>
     </ul>
-    <UButton label="Join Now" size="lg"/>
+    <UButton label="Join Now" color="blue" size="lg"/>
 </div>
 </template>
 
