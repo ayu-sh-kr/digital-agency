b1f2ca6b997038ac6f1e8bcf3d7e2f43a2acc113
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-16 00:45:47 +0530

    Update default.vue: Added AppFooter on layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 
 import AppHeader from "~/components/utils/AppHeader.vue";
+import AppFooter from "~/components/utils/AppFooter.vue";
 </script>
 
 <template>
     <AppHeader/>
     <slot/>
+    <AppFooter/>
     <UNotifications/>
 </template>
 
