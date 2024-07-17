c00ecbc62710c3e366ad61225e21e90cf5419c06
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-17 22:27:40 +0530

    Update OptionSection.vue: UI Fix for Font on header

--- components/home/OptionSection.vue
+++ components/home/OptionSection.vue
@@ -6,15 +6,15 @@
 <section class="px-3 py-10 font-tahoma w-full">
     <div class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 place-items-center gap-y-5 text-white w-full">
         <div class="option-base bg-green-500 hover:bg-green-700">
-            <h1 class="font-extrabold text-5xl">Ready to get started?</h1>
+            <h1 class="text-5xl">Ready to get started?</h1>
             <h3 class="font-semibold underline text-2xl cursor-pointer">Read our blogs</h3>
         </div>
         <div class="option-base bg-blue-500 hover:bg-blue-700">
-            <h1 class="font-extrabold text-5xl">Consider buying our course?</h1>
+            <h1 class="text-5xl">Consider buying our course?</h1>
             <h3 class="font-semibold underline text-2xl cursor-pointer">Discover Courses</h3>
         </div>
         <div class="option-base bg-teal-500 hover:bg-teal-700">
-            <h1 class="font-extrabold text-5xl">Want personal mentoring?</h1>
+            <h1 class="text-5xl">Want personal mentoring?</h1>
             <h3 class="font-semibold underline text-2xl cursor-pointer">Book a meet</h3>
         </div>
     </div>
